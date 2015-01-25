d07e3d6c82fdbf164e352200b6e5a0e221a242b0
jpecoraro342 <jpecoraro342@ufl.edu>
2015-01-25 17:49:17 -0500

    All the google translate stuff is working now

--- js/scripts.js
+++ js/scripts.js
@@ -39,25 +39,117 @@ $(document).ready(function(){
 function messageSendPressed() {
 	if (isLanguageSelected()) {
 		if (!isMessageBlank()) {
-			addMessageToList();
-			finishSendingMessage();
-			//TODO: Call translate and update ui
+			checkInputLanguage();
+			/*addMessageToList();
+			finishSendingMessage();*/	
 		}
 	}
 }
 
 //Translation Service
 
-function translateAndUpdateUI() {
+var translate_api_key = "AIzaSyAIM9ITnDye2arpTOupYnP-zRqZoZjkV2A";
+var translate_base_url = "https://www.googleapis.com/language/translate/v2";
 
+function translateAndUpdateUI(is_left_user, translate_origin, translate_destination) {
+	var message_text_area = document.getElementById("message-text");
+	var original_message = message_text_area.value;
+
+	$.get(translate_base_url, {
+		key:translate_api_key,
+		source:translate_origin,
+		target:translate_destination,
+		q:original_message 
+	},
+	function(response) {
+		var translated_text = response.data.translations[0].translatedText;
+		addMessageToList(is_left_user, translated_text);
+
+	}, "json") .fail(function(fail_object, text_status, error_thrown) {
+		console.log(fail_object + " - " + text_status + " - " + error_thrown);
+		alert( "Sorry, we were unable to translate the message. Error: " + error_thrown );
+	});
 }
 
 function checkInputLanguage() {
+	var message_text_area = document.getElementById("message-text");
+	var message = message_text_area.value;
 
+	$.get(translate_base_url + "/detect", {
+		key:translate_api_key,
+		q:message
+	},
+	function(response) {
+		var language_object = response.data.detections[0][0];
+		var lang = language_object.language;
+
+		var select1lang = document.getElementById("person1language").value; 
+		var select2lang = document.getElementById("person2language").value; 
+
+		if (lang == select1lang) {
+		//person on left - translate from lang 1 to lang 2
+		translateAndUpdateUI(true, select1lang, select2lang);
+		}
+		else if (lang == select2lang) {
+		//person on right - translate from lang 2 to lang
+		translateAndUpdateUI(false, select2lang, select1lang);
+		}
+		else {
+			alert("Sorry, we couldn't match the text to either of the selected languages \n\nUser 1 Language - " + select1lang + "\nUser 2 Language - " + select2lang + "\nDetected Language - " + lang);
+			return;
+		}
+	} ,"json") .fail(function(fail_object, text_status, error_thrown) {
+		console.log(fail_object + " - " + text_status + " - " + error_thrown);
+		alert( "Sorry, we were unable to translate the message. Error: " + error_thrown );
+	});
 }
 
-//Verificatoin
+//UI Updates
+function addMessageToList(is_left_user, translated_message) {
+	var message_text_area = document.getElementById("message-text");
+	var original_message = message_text_area.value;
 
+	var ul = document.getElementById("messsages-list");
+	var li = document.createElement("li");
+
+	var user_node_span = document.createElement("span");
+	user_node_span.setAttribute("class", "name-text");
+
+	var translated_node = document.createElement("span");
+	translated_node.setAttribute("class", "translate_text");
+	translated_node.appendChild(document.createTextNode(" [ " + translated_message + " ] "));
+
+	//To see if it is person 1 or 2 sending the message - temporary
+	if (is_left_user) {
+		li.setAttribute("class", "message col-xs-10 left-chat");
+		user_node_span.appendChild(document.createTextNode("User 1 - "));
+	}
+	else {
+		li.setAttribute("class", "message col-xs-10 col-xs-offset-2 right-chat");
+		user_node_span.appendChild(document.createTextNode("User 2 - "));
+	}
+	
+	li.appendChild(user_node_span);
+	li.appendChild(document.createTextNode(original_message));
+	li.appendChild(translated_node);
+
+	ul.appendChild(li);
+
+	finishSendingMessage();
+}
+
+function finishSendingMessage() {
+	//reset the text area
+	var message_text_area = document.getElementById("message-text");
+	message_text_area.value = "";
+
+	//scroll the scroll bar
+	$("#chat-frame").animate({
+		scrollTop:$("#chat-frame")[0].scrollHeight
+	}, 500);
+}
+
+//Verification
 function isLanguageSelected() {
 	var isInvalidSelect = false;
 	var select1 = document.getElementById("person1language"); 
@@ -100,40 +192,6 @@ function isMessageBlank() {
 
 	return false;
 }
-
-//UI Updates
-function addMessageToList() {
-	var message_text_area = document.getElementById("message-text");
-	var message = message_text_area.value;
-
-	var ul = document.getElementById("messsages-list");
-	var li = document.createElement("li");
-	
-	li.appendChild(document.createTextNode(message));
-
-	//To see if it is person 1 or 2 sending the message - temporary
-	if (Math.random() < .5) {
-		li.setAttribute("class", "message col-xs-10 left-chat");
-	}
-	else {
-		li.setAttribute("class", "message col-xs-10 col-xs-offset-2 right-chat");
-	}
-	
-	ul.appendChild(li);
-}
-
-function finishSendingMessage() {
-	//reset the text area
-	var message_text_area = document.getElementById("message-text");
-	message_text_area.value = "";
-
-	//scroll the scroll bar
-	$("#chat-frame").animate({
-		scrollTop:$("#chat-frame")[0].scrollHeight
-	}, 500);
-}
-
-
 
 
 
